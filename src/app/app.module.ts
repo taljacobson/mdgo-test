52908af350d39921faeb4724bfc6593d49b68796
taljacobson <tal1989@gmail.com>
2018-10-16 23:08:44 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SingleAccidentComponent } from './single-accident/single-accident.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SingleAccidentComponent
+    SingleAccidentComponent,
+    HeaderBarComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
