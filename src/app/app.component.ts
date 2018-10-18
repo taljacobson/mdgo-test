e19ea0e2fd172538ad003607b452c0068d7cdcb3
taljacobson <tal1989@gmail.com>
2018-10-18 18:46:21 +0300

    add sideMenuOpen state behaviorSubject and toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccidentService } from './accident.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mdgo-test';
+  sideMenuOpen = new BehaviorSubject<boolean>(true);
+  constructor(private accidentSrv: AccidentService) {}
+
+  ngOnInit() {
+    this.accidentSrv.initAcidents();
+  }
+
+  public toggleSideMenu() {
+    this.sideMenuOpen.next(!this.sideMenuOpen.value);
+  }
 }
