22db1e276ca74066826f13077720d7dcf6098dbd
taljacobson <tal1989@gmail.com>
2018-10-18 18:43:36 +0300

    add ability to change menu state through header-bar

--- src/app/header-bar/header-bar.component.ts
+++ src/app/header-bar/header-bar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header-bar',
   templateUrl: './header-bar.component.html',
-  styleUrls: ['./header-bar.component.scss']
+  styleUrls: ['./header-bar.component.scss'],
 })
 export class HeaderBarComponent implements OnInit {
+  @Output()
+  menuToggle = new EventEmitter();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onMenuToggle() {
+    this.menuToggle.emit();
   }
-
 }
